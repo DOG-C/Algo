46be35e970ac5826785e6929eb0f324fa00947aa
dogc <caoyuqi1996@hotmail.com>
2024-08-29 01:27:26 +0200

    a

--- src/main/java/com/dogie/algorithms/datastructures/linkedlist/DoubleLinkedList.java
+++ src/main/java/com/dogie/algorithms/datastructures/linkedlist/DoubleLinkedList.java
@@ -1,7 +1,26 @@
 package main.java.com.dogie.algorithms.datastructures.linkedlist;
 
 public class DoubleLinkedList<T> implements LinkedList<T> {
-    public void insertAfter(T elem) {
-        
+    private Node<T> front = null;
+    private Node<T> back = null;
+
+
+    private static class Node<T> {
+        private T value;
+        private Node<T> prev, next;
+
+        public Node(T value, Node<T> prev, Node<T> next) {
+            this.value = value;
+            this.prev = prev;
+            this.next = next;
+        }
+    }
+
+    public void insertAfter(Node<T> a, Node<T> x) {
+        Node<T> b = a.next;
+        x.prev = a;
+        x.next = b;
+        a.next = x;
+        b.prev = x;
     }
 }
